d3127298381a4c5c30a6892b60384fba576678c4
faisal-saddique <faisalsaddique7964@gmail.com>
2023-10-08 23:37:18 +0500

    added updates

--- pages/Retrieval_Agent.py
+++ pages/Retrieval_Agent.py
@@ -35,7 +35,7 @@ load_dotenv()
 client = Client()
 
 st.set_page_config(
-    page_title="ChatLangChain",
+    page_title="ChatDDW",
     page_icon="🦜",
     layout="wide",
     initial_sidebar_state="collapsed",
