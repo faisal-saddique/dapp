091fe508d423a62695cf4ff24dde929a455a0ece
faisal-saddique <faisalsaddique7964@gmail.com>
2023-10-09 11:02:52 +0500

    added updates

--- pages/Retrieval_Agent.py
+++ pages/Retrieval_Agent.py
@@ -16,12 +16,14 @@ from langchain.schema import SystemMessage, AIMessage, HumanMessage
 from langchain.prompts import MessagesPlaceholder
 from langsmith import Client
 from langchain.tools import BaseTool, StructuredTool, Tool, tool
+import pandas as pd
 
 from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI
 from langchain.agents.agent_types import AgentType
 
 from langchain.agents import create_csv_agent
+from langchain.agents.agent_toolkits.pandas.base import create_pandas_dataframe_agent
 
 from typing import Optional, Type
 
@@ -71,9 +73,11 @@ class GetAnswerForAnythingYouDontKnowAbout(BaseTool):
         self, query: str, run_manager: Optional[CallbackManagerForToolRun] = None
     ) -> str:
         """Use the tool."""
-        agent = create_csv_agent(
+        agent = create_pandas_dataframe_agent(
             ChatOpenAI(temperature=0, model="gpt-3.5-turbo-16k"),
-            ["./new data/DDW_Location_details_with_services_FINAL.csv", "./new data/Participants_FINAL.csv", "./new data/Programme_details_with_Narratives_and_Discipline_FINAL.csv"],
+            df=pd.read_csv("./new data/DDW_Location_details_with_services_FINAL.csv"),
+            return_intermediate_steps=True,
+            # ["./new data/DDW_Location_details_with_services_FINAL.csv", "./new data/Participants_FINAL.csv", "./new data/Programme_details_with_Narratives_and_Discipline_FINAL.csv"],
             verbose=True,
             agent_type=AgentType.OPENAI_FUNCTIONS,
         )
@@ -86,18 +90,7 @@ class GetAnswerForAnythingYouDontKnowAbout(BaseTool):
         """Use the tool asynchronously."""
         raise NotImplementedError("custom_search does not support async")
 
-csv_tool = Tool(
-    name="DDW_location_details_with_services",
-    description="Use this tool whenever you need to answer anything about Places_Name, Location Photo, Google Maps link, Places_Latitude, Places_Longitude, Places_Address, Places_City, Places_PostalCode, Opening times, Services	Dogs allowed, Fully Wheelchair Accessible, Partially Wheelchair Accessible, Toilets available, Wheelchair Friendly Toilet, Wifi available",
-    func=create_csv_agent(
-            ChatOpenAI(temperature=0, model="gpt-3.5-turbo-16k"),
-            "./new data/DDW_Location_details_with_services_FINAL.csv",
-            verbose=True,
-            agent_type=AgentType.OPENAI_FUNCTIONS,
-        )
-)
-
-tools = [tool_ddw_and_tickets_queries,csv_tool]
+tools = [tool_ddw_and_tickets_queries,GetAnswerForAnythingYouDontKnowAbout()]
 
 llm = ChatOpenAI(temperature=0, streaming=True, model="gpt-3.5-turbo-16k")
 
